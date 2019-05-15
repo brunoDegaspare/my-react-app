565b917a432b29fb44f506901b37289ab91836ee
Bruno Degaspare <bruno@sitebrunodias.com>
2019-05-15 22:45:49 +0100

    Clean style

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Learn how to design and code React apps.</h1>
     <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Link to="/page-2/">Go to next page</Link>
   </Layout>
 )
