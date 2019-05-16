d63f041ff09f527fd8956481841b8be999a119ec
Bruno Degaspare <bruno@sitebrunodias.com>
2019-05-16 07:40:57 +0100

    style hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,14 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Learn how to design and code React apps.</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <Link to="/page-2/">Go to next page</Link>
+    <SEO title="My First React app" keywords={[`design`, `application`, `react`]} />
+    <div className="hero">
+      <div className="hero__group">
+        <h1 className="hero__heading">Learn how to design and code React apps.</h1>
+        <p className="hero__paragraph">Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <Link className="hero__cta" to="/page-2/">Go to next page</Link>
+      </div>    
+    </div>
   </Layout>
 )
 
