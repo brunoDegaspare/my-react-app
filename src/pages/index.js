449cef7ac8fac9caea9a06a81c38cba51122592c
Bruno Degaspare <brunodegaspare@UK0234-BRUNO-DEGASPARE.local>
2019-05-17 15:14:48 +0100

    Add interactions and animations to hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
     <Header />
     <div className="hero">
       <div className="hero__group">
-        <h1 className="hero__heading">Learn how to design and code React apps.</h1>
+        <h1 className="hero__heading">Learn how to <br/> design and code React apps.</h1>
         <p className="hero__paragraph">Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
         <Link className="hero__cta" to="/page-2/">Go to next page</Link>
       </div>    
