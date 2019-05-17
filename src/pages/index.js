4c5cbc35049de67a8527550085488d29060870e0
Bruno Degaspare <brunodegaspare@UK0234-BRUNO-DEGASPARE.local>
2019-05-17 15:33:54 +0100

    Change layout structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../layout/layout"
 import Header from "../components/header"
 import Image from "../components/image"
 import SEO from "../components/seo"
